facf1ca41836bff7070a6b056c20d1ae141d7805
juzichas <juzichas@gmail.com>
2020-10-25 20:37:47 -0400

    changes for final build

---
+++ PlatformerTest/Assets/transition stuff/menuFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public void startGame() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+               
+    }
+    public void quit() {
+
+        Application.Quit(); 
+    
+    }
+}
